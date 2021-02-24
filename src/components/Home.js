999732ecd16f84ee84119ceb4fa16df808f70002
tobil <tobilesi@gmail.com>
2021-02-24 03:33:31 +0000

    home page components are there needs proper sizing

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,24 @@ function Home() {
 
     <>
      <header className="homePage-wrapper">
-        <div>
-          Time to start coding!
+        <div className="title">
+          <h1>Tobi Lesi</h1>
+          <h3>Junior Software Developer</h3>
+        </div>
+        <div className="aboutMe-wrapper">
+          <h4 className="aboutMe-title">About Me</h4>
+          <div className="ui divider aboutMe-div"></div>
+          <div className="aboutMe-content">
+            <div className="first-section">
+              My dedication to programming came whilst I was studying my Bachelor in Computer Science. My interest began spending time outside the class to learn the fundamentals of Web Development on Udemy.
+            </div>
+            <div className="second-section">
+              Upon furthering my knowledge by graduating from General Assembly, I have been exposed to the depth and breadth of the industry. In turn, this generated an eagerness of having in-depth knowledge on both the front-end and the back-end as well as consolidating the foundations I have built from both university and GA.
+            </div>
+            <div className="third-section">
+              I am looking for a team where we can be constantly challenging each other to meet the loft ambitions that we strategise towards. In order to highlight my unwavering determination to reach the goals set in front of me.
+            </div>
+          </div>
         </div>
      </header>
     </>
