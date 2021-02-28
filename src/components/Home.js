1557537f22bfe1aa0d4b169c531d1dc8e9afd23b
tobil <tobilesi@gmail.com>
2021-02-28 15:31:43 +0000

    home page responsive with no contact footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import Nav from './Nav'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedinIn, faDribbble, faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faLinkedinIn, faDribbble, faGithub } from '@fortawesome/free-brands-svg-icons'
+// import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 
 function Home() {
 
   return (
 
     <>
-      <header className="homePage-wrapper">
-        <div className="title">
-          <h1>Tobi Lesi</h1>
-          <h3>Junior Software Engineer</h3>
-        </div>
-        <section className="contact-wrapper">
-          {/* <div className="contact-title">
+      <section className="homePage-wrapper">
+        <header>
+          <div className="title">
+            <h1>Tobi Lesi</h1>
+            <h3>Junior Software Engineer</h3>
+          </div>
+          {/* <section className="contact-wrapper">
+          <div className="contact-title">
           <h4>Feel free to get in touch !!</h4>
-        </div> */}
+        </div>
           <div className="contact-content">
             <a href="https://www.linkedin.com/in/oluwatobi-lesi/" >
               <div className="contact-social">
@@ -41,10 +42,10 @@ function Home() {
               </div>
             </a>
           </div>
-        </section>
-      </header>
-      <Nav />
-
+        </section> */}
+        </header>
+        <Nav />
+      </section>
     </>
   )
 
